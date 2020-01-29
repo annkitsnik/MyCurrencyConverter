81c8960bb0ad6b4dff05fd903ad8daf67a616e0a
Ann Kitsnik <annkitsnik@hot.ee>
2020-01-29 11:04:43 +0200

    first commit

---
+++ MyCurrencyConverter/CurrencyDetails.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyCurrencyConverter
+{
+    public class Rates
+    {
+        
+        
+        public double AUD { get; set; }
+        
+        public double EGP { get; set; }
+
+        public double EUR { get; set; }
+        public double GBP { get; set; }
+
+        public double NOK { get; set; }
+        public double NZD { get; set; }
+
+
+
+    }
+
+    public class RootObject
+    {
+        public string disclaimer { get; set; }
+        public string license { get; set; }
+        public int timestamp { get; set; }
+        public string @base { get; set; }
+        public Rates rates { get; set; }
+    }
+}
+
---
+++ MyCurrencyConverter/Program.cs
@@ -0,0 +1,84 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Net;
+
+namespace MyCurrencyConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            bool gameOver = false;
+
+            Console.WriteLine("Welcome to our currency converter!\n");
+
+            string url = "https://openexchangerates.org/api/latest.json?app_id=b05a9cc2604b409aab098cf79b143034";
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "GET";
+
+            var webResponse = request.GetResponse();
+            var webStream = webResponse.GetResponseStream();
+
+            using (var responseReader = new StreamReader(webStream))
+            {
+                var response = responseReader.ReadToEnd();
+                Rates rates = JsonConvert.DeserializeObject<Rates>(response);
+                //Console.WriteLine(response); //hack
+
+                RootObject rootObject = JsonConvert.DeserializeObject<RootObject>(response);
+
+                Console.WriteLine($"Base currency: {rootObject.@base}");
+                Console.WriteLine($"Timestamp: {rootObject.timestamp}\n");
+
+                Console.WriteLine("Today's rates are as following:\n ");
+                Console.WriteLine($"Australian dollar: {rootObject.rates.AUD}");
+                Console.WriteLine($"Egyptian Pound: {rootObject.rates.EGP}");
+                Console.WriteLine($"Euro: {rootObject.rates.EUR}");
+                Console.WriteLine($"British Pound: {rootObject.rates.GBP}");
+                Console.WriteLine($"Norwegian Crone: {rootObject.rates.NOK}");
+                Console.WriteLine($"New Zealand Dollar: {rootObject.rates.NZD}\n");
+
+                Start:
+                Console.WriteLine("Enter your amount in USD: ");
+                int userInput= Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("\n");
+                Console.WriteLine("The amount is: \n");
+
+                Console.WriteLine($"Australian dollar: {userInput * rootObject.rates.AUD}");
+                Console.WriteLine($"Egyptian Pound: {userInput * rootObject.rates.EGP}");
+                Console.WriteLine($"Euro: {userInput * rootObject.rates.EUR}");
+                Console.WriteLine($"British Pound: {userInput * rootObject.rates.GBP}");
+                Console.WriteLine($"Norwegian Crone: {userInput * rootObject.rates.NOK}");
+                Console.WriteLine($"New Zealand Dollar: {userInput * rootObject.rates.NZD}\n");
+
+                Console.WriteLine("Would you like to calculate again? y/n");
+                string userAnswer = Console.ReadLine();
+
+                if (userAnswer.ToLower() == "y")
+                {
+                    goto Start;
+                }
+                else if(userAnswer.ToLower()=="n")
+                {
+                    
+                    Console.WriteLine("See you soon!");
+                    gameOver = true;
+                   
+                }
+                else
+                {
+                    Console.WriteLine("\nCheck your input!");
+                    goto Start;
+                }
+
+
+
+            }
+            Console.ReadLine();
+        }
+
+    }
+}
